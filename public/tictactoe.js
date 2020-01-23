3de40f2b939c16cc08438257d026647980d5fa06
glptrst <giulio.pietroiusti@gmail.com>
2020-01-24 00:52:10 +0100

    Replace prompt with modal window.

--- public/tictactoe.js
+++ public/tictactoe.js
@@ -1,98 +1,111 @@
 'use strict';
-let roomNumber = window.prompt('Choose room number:');
 
-const HOST = location.origin.replace(/^http/, 'ws');
-const ws = new WebSocket(HOST);
+let roomNumber;
 
-ws.onopen = function() {
-  console.log('WebSocket Client Connected');
-  ws.send(JSON.stringify({
-    type: 'joinRoom',
-    roomNumber: roomNumber,
-  }));
-};
-ws.onmessage = function(e) {
-  //console.log(e.data);
-  let action = JSON.parse(e.data);
+document.getElementById('roomButton').addEventListener('click', () => {
+  document.querySelector('.modal-bg').style.display = 'none';
+  roomNumber = document.getElementsByTagName('input')[0].value;
+  startApp(roomNumber);
+});
 
-  if (action.type === 'update') {
-    document.getElementById('board').textContent = '';
-    document.getElementById('board').appendChild(renderBoard(action.board));
-    let buttons = document.getElementsByTagName('button');
-    for (let i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener('click', () => {
-	ws.send(JSON.stringify({
-	  type: 'move',
-	  square: buttons[i].id,
-	  roomNumber: roomNumber,
-	}));
-      });
-    }
+//let roomNumber = window.prompt('Choose room number:');
 
-    document.getElementById('status').textContent = '';
-    let status = document.createTextNode(`Status of the game: ${action.status}`);
-    document.getElementById('status').appendChild((status));
+function startApp(roomNumber) {
+  const HOST = location.origin.replace(/^http/, 'ws');
+  const ws = new WebSocket(HOST);
 
-    if (action.room) {
-      document.getElementById('room').textContent = '';
-      let roomText = document.createTextNode(`You are in room ${action.room}`);
-      document.getElementById('room').appendChild((roomText));
-    }
-    if (action.mark) {
-      document.getElementById('mark').textContent = '';
-      let markText = document.createTextNode(`Your mark is ${action.mark}`);
-      document.getElementById('mark').appendChild((markText));
-    }
-    if (action.newGame) {
+  ws.onopen = function() {
+    console.log('WebSocket Client Connected');
+    ws.send(JSON.stringify({
+      type: 'joinRoom',
+      roomNumber: roomNumber,
+    }));
+  };
+  ws.onmessage = function(e) {
+    //console.log(e.data);
+    let action = JSON.parse(e.data);
+
+    if (action.type === 'update') {
+      document.getElementById('board').textContent = '';
+      document.getElementById('board').appendChild(renderBoard(action.board));
+      let buttons = document.getElementsByTagName('button');
+      for (let i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener('click', () => {
+	  ws.send(JSON.stringify({
+	    type: 'move',
+	    square: buttons[i].id,
+	    roomNumber: roomNumber,
+	  }));
+	});
+      }
+
       document.getElementById('status').textContent = '';
-      let status = document.createTextNode(`Status of the game: ${action.status}. `);
-      let link = document.createElement('span');
-      let linkTxt = document.createTextNode('Click here to start a new game');
-      link.appendChild(linkTxt);
-      document.getElementById('status').addEventListener('click', () => {
-	ws.send(JSON.stringify({
-	  type: 'newGame',
-	  roomNumber: roomNumber,
-	}));
-      });
+      let status = document.createTextNode(`Status of the game: ${action.status}`);
       document.getElementById('status').appendChild((status));
-      document.getElementById('status').appendChild((link));
-    }
-  } else if ('resetBoard') {
-    document.getElementById('board').textContent = '';
-    document.getElementById('board').appendChild(renderBoard(action.board));
-    let buttons = document.getElementsByTagName('button');
-    for (let i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener('click', () => {
-	ws.send(JSON.stringify({
-	  type: 'move',
-	  square: buttons[i].id,
-	  roomNumber: roomNumber,
-	}));
-      });
-    }
 
-    document.getElementById('status').textContent = '';
-    let status = document.createTextNode(`New Game started! ${action.status}`);
-    document.getElementById('status').appendChild((status));
-  } else if (action.type === 'room number error') {
-    roomNumber = window.prompt('You must insert a number:');
-    ws.send(JSON.stringify({
-      type: 'joinRoom',
-      roomNumber: roomNumber,
-    }));
-  } else if (action.type === 'room full') {
-    roomNumber = window.prompt('Room full, try another number:');
-    ws.send(JSON.stringify({
-      type: 'joinRoom',
-      roomNumber: roomNumber,
-    }));
-  }
-};
+      if (action.room) {
+	document.getElementById('room').textContent = '';
+	let roomText = document.createTextNode(`You are in room ${action.room}`);
+	document.getElementById('room').appendChild((roomText));
+      }
+      if (action.mark) {
+	document.getElementById('mark').textContent = '';
+	let markText = document.createTextNode(`Your mark is ${action.mark}`);
+	document.getElementById('mark').appendChild((markText));
+      }
+      if (action.newGame) {
+	document.getElementById('status').textContent = '';
+	let status = document.createTextNode(`Status of the game: ${action.status}. `);
+	let link = document.createElement('span');
+	let linkTxt = document.createTextNode('Click here to start a new game');
+	link.appendChild(linkTxt);
+	document.getElementById('status').addEventListener('click', () => {
+	  ws.send(JSON.stringify({
+	    type: 'newGame',
+	    roomNumber: roomNumber,
+	  }));
+	});
+	document.getElementById('status').appendChild((status));
+	document.getElementById('status').appendChild((link));
+      }
+    } else if ('resetBoard') {
+      document.getElementById('board').textContent = '';
+      document.getElementById('board').appendChild(renderBoard(action.board));
+      let buttons = document.getElementsByTagName('button');
+      for (let i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener('click', () => {
+	  ws.send(JSON.stringify({
+	    type: 'move',
+	    square: buttons[i].id,
+	    roomNumber: roomNumber,
+	  }));
+	});
+      }
 
-let moves = [null, null, null,
-	     null, null, null,
-	     null, null, null];
+      document.getElementById('status').textContent = '';
+      let status = document.createTextNode(`New Game started! ${action.status}`);
+      document.getElementById('status').appendChild((status));
+    } else if (action.type === 'room number error') {
+      roomNumber = window.prompt('You must insert a number:');
+      ws.send(JSON.stringify({
+	type: 'joinRoom',
+	roomNumber: roomNumber,
+      }));
+    } else if (action.type === 'room full') {
+      roomNumber = window.prompt('Room full, try another number:');
+      ws.send(JSON.stringify({
+	type: 'joinRoom',
+	roomNumber: roomNumber,
+      }));
+    }
+  };
+
+  let moves = [null, null, null,
+	       null, null, null,
+	       null, null, null];
+}
+
+
 
 function renderBoard(moves) {
   let board = document.createElement('div');
