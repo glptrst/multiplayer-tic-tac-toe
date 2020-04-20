ce1ca2953d78bc0d1f9ab86c4926935e154be85f
glptrst <giulio.pietroiusti@gmail.com>
2020-04-20 22:59:43 +0200

    undo last change.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const WebSocket = require('ws');
 /*             */
 /* HTTP SERVER */
 /*             */
-const httpServer = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
 
   let filePath = '.' + req.url;
   if (filePath === './')
@@ -58,7 +58,7 @@ const httpServer = http.createServer((req, res) => {
 /*                  */
 /* WEBSOCKET SERVER */
 /*                  */
-const wss = new WebSocket.Server({ httpServer });
+const wss = new WebSocket.Server({ server });
 
 let rooms = [
   // {
